0d3474bc4722ea0ef72bf7fe2601a7bac3b60621
jayden <jxdjayden@gmail.com>
2018-04-24 18:09:58 +0800

    0.2.0

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const install = function(Vue, opts = {}) {
     Vue.component(DrawBoard.name, DrawBoard);
 };
 
-const version = '0.1.0';
+const version = '0.2.0';
 
 export { DrawBoard, install, version };
 export default { DrawBoard, install, version };
